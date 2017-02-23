5896dc54da5fc2c3d078c1ee59f6956747d7db4e
Rhys Puddephatt <rhys.puddephatt@btinternet.com>
2017-02-23 15:12:24 +0000

    first implimentation of factory

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,26 @@
 import Controllers.DashboardMainFrameController;
+import DataStructures.ClickLog;
+import DataStructures.CsvInterfaces.IClickLog;
+import DataStructures.CsvPaserFactory;
 import Views.DashboardMainFrame;
 
 import javax.swing.*;
 import java.io.File;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
+        CsvPaserFactory factory = new CsvPaserFactory();
+        try {
+            String path = "/home/rhys/SegProj/AuctionDashboard/click_log.csv";
+            factory.open(ClickLog.class, path);
+            IClickLog c = (IClickLog) factory.next();
+
+            System.out.println(c);
+            factory.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         DashboardMainFrame frame = new DashboardMainFrame(new File(System.getProperty("user.home")));
         DashboardMainFrameController controller = new DashboardMainFrameController(frame);
         //TODO resolve the circular dependency with the listener pattern
