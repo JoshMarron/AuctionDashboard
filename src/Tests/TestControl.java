5aefb4e120ff3d539539c13af10470035909ef2a
Eruantiel <mateuszmarton1@gmail.com>
2017-02-23 14:21:53 +0000

    Added main class for future testing.

---
+++ src/Tests/TestControl.java
@@ -0,0 +1,10 @@
+package Tests;
+
+/**
+ * Created by mati on 23/02/2017.
+ */
+public class TestControl {
+    public static void main(String args[]){
+        System.out.println("TESTING...");
+    }
+}
