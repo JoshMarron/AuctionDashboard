dfd7fae266feace5b5a29bf902d933c5c31e1491
Eruantiel <mateuszmarton1@gmail.com>
2017-03-01 20:53:51 +0000

    Added 8 tests for metricUtils

--- src/Tests/TestControl.java
+++ src/Tests/TestControl.java
@@ -6,33 +6,44 @@ import org.junit.Test;
 import org.junit.runner.notification.RunListener;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 /**
  * Created by mati on 23/02/2017.
  */
 public class TestControl {
 
+//    @Test
+//    public void testNormal1CalculateCTR(){
+//        assertEquals("should be 20.0", 0.05, MetricUtils.calculateCTR(5 , 100), 0.00005);
+//    }
+//    @Test
+//    public void testNormal2CalculateCTR(){
+//        assertEquals("should be 20.0", 0.0507, MetricUtils.calculateCTR(5.07 , 100), 0.00005);
+//    }
     @Test
-    public void testJunit() {
-        System.out.println("Testing begun...");
-        boolean i = true;
-        assertEquals("Junit should work (should be true)", i, true);
-    }
-
-    @Test
-    public void testNormalCalculateCTR(){
-        assertEquals("should be 20.0", 20.0, MetricUtils.calculateCTR(100 , 5), 0.00005);
-
-    }
-    @Test
-    public void testBoundaryCalculateCTR(){
-        assertEquals("should be 20.0", 16.66667, MetricUtils.calculateCTR(100 , 6), 0.00005);
-
+    public void testNormal3CalculateCTR(){
+        assertEquals("should be 20.0", 0.0606, MetricUtils.calculateCTR(6 , 99), 0.00005);
     }
     @Test
-    public void testBoundaryCalculateCTR(){
-        assertEquals("should be 20.0", 20.0, MetricUtils.calculateCTR(100 , 5), 0.00005);
-
+    public void testBoundaryLowCalculateCTR(){
+        assertEquals("should be 20.0", 0.0, MetricUtils.calculateCTR(0 , 6), 0.00005);
+    }
+    @Test
+    public void testBoundaryHighCalculateCTR(){
+        assertEquals("should be 20.0", 1.0, MetricUtils.calculateCTR(6 , 6), 0.00005);
+    }
+    @Test
+    public void testOutOfBoundary1CalculateCTR(){
+        assertEquals("should be 20.0", 1.5, MetricUtils.calculateCTR(6 , 4), 0.00005);
+    }
+    @Test
+    public void testOutOfBoundary2CalculateCTR(){
+        assertEquals("should be 20.0", -1.5, MetricUtils.calculateCTR(6 , -4), 0.00005);
+    }
+    @Test
+    public void testErroneousCalculateCTR(){
+            assertEquals("should be infite",true,  Float.isInfinite(MetricUtils.calculateCTR(100, 0)));
     }
 
 
