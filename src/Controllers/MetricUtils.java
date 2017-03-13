90a4a7464de2f5db33333a522a3cd2d791a240e4
Joe Preddy <joe.preddy@gmail.com>
2017-03-13 12:46:17 +0000

    Added a ton more metrics :sunglasses:

--- src/Controllers/MetricUtils.java
+++ src/Controllers/MetricUtils.java
@@ -2,6 +2,7 @@ package Controllers;
 
 import Model.TableModels.Click;
 import Model.TableModels.Impression;
+import Model.TableModels.ServerVisit;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,5 +25,42 @@ public class MetricUtils {
 
     public static double getCostPerClick(List<Double> clickCost, int noOfClicks){
         return calculateTotalCost(clickCost)/noOfClicks;
+    }
+
+    public static int getBounceCount(List<ServerVisit> log){
+        int count = 0;
+        for(ServerVisit visit : log){
+            if (visit.getPagesViewed() == 1){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static double getBounceRate(List<Click> clicks, List<ServerVisit> visits){
+        return (getBounceCount(visits)/clicks.size());
+    }
+
+    public static int getConversionCount(List<ServerVisit> serverLog){
+        int count = 0;
+        for(ServerVisit visit : serverLog){
+            if(visit.isConversion()){
+                count ++;
+            }
+        }
+        return count;
+    }
+
+    public static double getCostPerAcquisition(List<ServerVisit> serverLog, List<Double> clickCost){
+        return (calculateTotalCost(clickCost)/getConversionCount(serverLog));
+    }
+
+    public static double getCostPerImpression(List<Impression> impressions, List<Double> clickCosts){
+        return(calculateTotalCost(clickCosts)/getImpressionCount(impressions));
+    }
+
+    //CPM
+    public static double getCostPerThousandImpressions(List<Impression>impressions, List<Double> clickCosts){
+        return (getCostPerImpression(impressions, clickCosts)/1000);
     }
 }
