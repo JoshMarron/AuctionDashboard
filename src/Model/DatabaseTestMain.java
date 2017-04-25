8b4e77efa0c03e65b44627aff502625bc0b158c4
Phil <philipcumner@gmail.com>
2017-04-25 15:40:30 +0100

    Add join on site_impression as context threw an error

--- src/Model/DatabaseTestMain.java
+++ src/Model/DatabaseTestMain.java
@@ -9,6 +9,7 @@ import Model.DBEnums.attributes.Attribute;
 import Model.DBEnums.attributes.IncomeAttribute;
 import Views.MetricType;
 import Views.ViewPresets.AttributeType;
+import org.w3c.dom.Attr;
 
 import javax.xml.crypto.Data;
 import java.sql.SQLException;
@@ -43,9 +44,10 @@ public class DatabaseTestMain {
 		map.put(AttributeType.GENDER, Arrays.asList("Male", "Female"));
 		map.put(AttributeType.INCOME, Arrays.asList("Low"));
 		map.put(AttributeType.AGE, Arrays.asList("25-34", "35-44"));
+		map.put(AttributeType.CONTEXT, Arrays.asList("Social Media"));
 		System.out.println("map: " + map);
 
-		TimeDataQuery q = new TimeQueryBuilder(MetricType.CTR).filters(map).build();
+		TimeDataQuery q = new TimeQueryBuilder(MetricType.CPM).filters(map).build();
 		System.out.println("query: " + q.getFilters());
 //
 //		System.out.println(model.setBetween(q, "click_date"));
